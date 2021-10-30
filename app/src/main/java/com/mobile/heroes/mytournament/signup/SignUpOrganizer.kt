f38b0fe16c8ec3c86df14472b4305ee91c7d2055
bmigue <47197646+bmigue@users.noreply.github.com>
2021-10-29 19:22:20 -0600

    Adding view of Sign Up organizer

---
+++ app/src/main/java/com/mobile/heroes/mytournament/signup/SignUpOrganizer.kt
@@ -0,0 +1,12 @@
+package com.mobile.heroes.mytournament.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobile.heroes.mytournament.R
+
+class SignUpOrganizer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_organizer)
+    }
+}
