75ba828f6f432abe1b601c2e9efdca93bba4afb8
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2021-11-02 17:17:06 -0600

    Trabajo en el contador de partidos
    Se deja casi lista la parte gráfica de la historia de usuario

---
+++ app/src/main/java/com/mobile/heroes/mytournament/soccer_scoreboard.kt
@@ -0,0 +1,11 @@
+package com.mobile.heroes.mytournament
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class soccer_scoreboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soccer_scoreboard)
+    }
+}
