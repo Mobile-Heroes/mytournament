8f21ceb1d6d4e23ff1a168d7ea437a3bc52f6d2e
Rogelio <rsanchezc@ucenfotec.ac.cr>
2021-11-02 14:04:50 -0600

    start of HU-4

---
+++ app/src/main/java/com/mobile/heroes/mytournament/FeedViewHolder.kt
@@ -0,0 +1,10 @@
+package com.mobile.heroes.mytournament
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class FeedViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+
+}
