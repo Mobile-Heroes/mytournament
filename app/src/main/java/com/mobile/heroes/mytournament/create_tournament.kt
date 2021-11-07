2c5b77217a16ed71f199d94264200bab210997e4
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2021-11-07 00:07:55 -0600

    Creación de la US de registro de torneo

---
+++ app/src/main/java/com/mobile/heroes/mytournament/create_tournament.kt
@@ -0,0 +1,11 @@
+package com.mobile.heroes.mytournament
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_tournament : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_tournament)
+    }
+}
