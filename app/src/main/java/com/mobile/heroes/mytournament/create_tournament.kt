7ab2ef6aa076c2fa387de360034a98d76594dbbc
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2021-11-09 15:32:15 -0600

    Agregar parte lógica de la funcionalidad

--- app/src/main/java/com/mobile/heroes/mytournament/create_tournament.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/create_tournament.kt
@@ -4,26 +4,40 @@ import android.icu.number.NumberFormatter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.widget.*
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class create_tournament : AppCompatActivity() {
 
-    lateinit var tilTournamentType: TextInputLayout
+    lateinit var tietTournamentName: TextInputEditText
+    lateinit var tietGroupQuantity: TextInputEditText
     lateinit var actvSelectStrategy: AutoCompleteTextView
     lateinit var strategyTournament: Array<String>
     lateinit var itemAdapter: ArrayAdapter<String>
+    lateinit var btnCancel: Button
+    lateinit var btnAccept: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_tournament)
 
-        tilTournamentType = findViewById(R.id.tilTournamentType)
-        actvSelectStrategy = findViewById(R.id.actvSelectStrategy)
+        var groupQuantity: Int = 0
+        var tournamentName: String = ""
+        var selectStrategy: String = ""
 
-        strategyTournament = arrayOf<String>("item1", "item2", "item3")
+        tietTournamentName = findViewById(R.id.tietTournamentName)
+        tietGroupQuantity = findViewById(R.id.tietGroupQuantity)
+        actvSelectStrategy = findViewById(R.id.actvSelectStrategy)
+        btnCancel = findViewById(R.id.btnCancel)
+        btnAccept = findViewById(R.id.btnAccept)
+
+        //Se genera el dropdown de formatos de torneos
+        strategyTournament = arrayOf<String>(
+            "Tabla General",
+            "Grupos",
+            "Eliminación"
+        )
         itemAdapter = ArrayAdapter<String>(
             this,
             R.layout.dropdown_tournament_types,
@@ -32,6 +46,18 @@ class create_tournament : AppCompatActivity() {
         actvSelectStrategy.setAdapter(itemAdapter)
         actvSelectStrategy.setOnItemClickListener { p0, p1, p2, p3 ->
             val selectedItem: String? = p0?.toString()
+        }
+
+        btnAccept.setOnClickListener{
+            try {
+                groupQuantity = tietGroupQuantity.text.toString().toInt()
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(
+                    applicationContext,
+                    "Por favor escribir un número valido, sin caracteres especiales",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
