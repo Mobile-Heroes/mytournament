9c8e9a6917290d769767eb3e909d5b4d6e7e0053
Rogelio <rsanchezc@ucenfotec.ac.cr>
2021-11-09 17:28:31 -0600

    added sample data feed recivle view

--- app/src/main/java/com/mobile/heroes/mytournament/MainActivity.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/MainActivity.kt
@@ -9,12 +9,19 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobile.heroes.mytournament.databinding.ActivityMainBinding
+import com.mobile.heroes.mytournament.feed.FeedAdapter
+import kotlinx.android.synthetic.main.fragment_feed_destination.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var feedTitleList = mutableListOf<String>()
+    private var feedDescriptionList = mutableListOf<String>()
+    private var feedUserList = mutableListOf<String>()
+    private var feedImageList = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +43,23 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        postToFeedList()
+
+        rv_feed_card.layoutManager = LinearLayoutManager(this)
+        rv_feed_card.adapter = FeedAdapter(feedTitleList, feedDescriptionList, feedUserList, feedImageList)
+    }
+
+    private fun addToList(title:String, description:String, user:String, image:Int){
+        feedTitleList.add(title)
+        feedDescriptionList.add(description)
+        feedUserList.add(user)
+        feedImageList.add(image)
+    }
+
+    private fun postToFeedList(){
+        for(i:Int in 1..10){
+            addToList("Título Torneo $i", "Descripción del Torneo $i", "organizador $i", R.drawable.ic_tournament_image)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/mobile/heroes/mytournament/feed/FeedAdapter.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/feed/FeedAdapter.kt
@@ -1,22 +1,28 @@
 package com.mobile.heroes.mytournament.feed
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.mobile.heroes.mytournament.R
 
-class FeedAdapter(private val images: List<String>) : RecyclerView.Adapter<FeedViewHolder>() {
+class FeedAdapter(private var titles: List<String>, private var descriptions: List<String>,
+                  private var organizer:List<String>, var images: List<Int>) :
+    RecyclerView.Adapter<FeedViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return FeedViewHolder(layoutInflater.inflate(R.layout.item_feed, parent, false))
     }
 
-    override fun getItemCount(): Int = images.size
-
+    override fun getItemCount(): Int {
+            return titles.size
+        }
 
     override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
-        val item = images[position]
-        holder.bind(item)
+        holder.itemTitle.text = titles[position]
+        holder.itemDescription.text = descriptions[position]
+        holder.itemUser.text = organizer[position]
+        holder.itemPicture.setImageResource(images[position])
     }
 }
