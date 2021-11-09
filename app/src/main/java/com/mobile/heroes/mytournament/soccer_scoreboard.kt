23411ec222ec486ccfbdb7db1a2a5f32eca48103
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2021-11-08 23:34:03 -0600

    Ultimos cambios hasta el momento

--- app/src/main/java/com/mobile/heroes/mytournament/soccer_scoreboard.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/soccer_scoreboard.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
+import com.google.android.material.textfield.TextInputEditText
 import com.google.gson.Gson
 import com.mobile.heroes.mytournament.networking.ApiClient
 import com.mobile.heroes.mytournament.networking.services.FieldResource.FieldResource
@@ -32,68 +33,63 @@ import java.util.*
 
 class soccer_scoreboard : AppCompatActivity() {
 
-
-    lateinit var btnCancel: Button
-    lateinit var btnAccept: Button
     private lateinit var sessionManager: SessionManager
     private lateinit var apiClient: ApiClient
 
     lateinit var tvDateTime: TextView
     lateinit var tvUbication: TextView
-    lateinit var etHomeTeam: EditText
-    lateinit var etVisitorTeam: EditText
     lateinit var ivHomeTeam: ImageView
     lateinit var ivVisitorTeam: ImageView
-    lateinit var tvHomeTeam: TextView
-    lateinit var tvVisitorTeam: TextView
-    lateinit var ivIconLessPointH: ImageView
-    lateinit var ivIconPlusPointH: ImageView
-    lateinit var ivIconLessPointV: ImageView
-    lateinit var ivIconPlusPointV: ImageView
-    lateinit var tvCounterH: TextView
-    lateinit var tvCounterV: TextView
+    lateinit var tieScoreH: TextInputEditText
+    lateinit var tieScoreV: TextInputEditText
     lateinit var btnCancel: Button
     lateinit var btnAccept: Button
-
-
-
+    var pointH: Int = 0
+    var pointV: Int = 0
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_soccer_scoreboard)
 
         apiClient = ApiClient()
         sessionManager = SessionManager(this)
 
+        tvDateTime = findViewById(R.id.tvDateTime)
+        tvUbication = findViewById(R.id.tvUbication)
+        ivHomeTeam = findViewById(R.id.ivHomeTeam)
+        ivVisitorTeam = findViewById(R.id.ivVisitorTeam)
+        tieScoreH = findViewById(R.id.tieScoreH)
+        tieScoreV = findViewById(R.id.tieScoreV)
+        btnCancel = findViewById(R.id.btnCancel)
+        btnAccept = findViewById(R.id.btnAccept)
 
-        apiClient.getApiService().login(LoginRequest(username = "admin", password = "admin", rememberMe = false))
+        apiClient.getApiService()
+            .login(LoginRequest(username = "admin", password = "admin", rememberMe = false))
             .enqueue(object : Callback<LoginResponse> {
                 override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                println("error")
+                    //TODO Obtener el partido en proceso o finalizado desde la BD
+                    //TODO Agregar los datos de fecha y hora desde  la BD
+                    //TODO Agregar la imagen de los logos de los escudos de lo equipos del partido
+                    //TODO En base al partido seleccionado, se deberá de agregar puntajes al equipo y al torneo (tabla general)
+                    //TODO: ESTA VARA FUNCIONA LA VARA ES QUE PORQUE LOS POJOS DE TOURNAMENT Y FIELD NO TIENEN LOS CAMPOS
+                    // NESESARIOS CORREJIT PARA EVITAR EL ERROR
                 }
 
-                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                override fun onResponse(
+                    call: Call<LoginResponse>,
+                    response: Response<LoginResponse>
+                ) {
                     val loginResponse = response.body()
 
                     if (loginResponse?.id_token != null) {
                         sessionManager.saveAuthToken(loginResponse.id_token)
                     } else {
-                println("Error")
+                        println("Error")
                     }
                 }
             })
-
-        /**
-         * Esta sección meramente es el manejor de botones de la aplicación (definición e inicialización), a este punto no están relevante,
-         * es lo que nos permite sumar o restar goles y enviar data a la DB
-         */
-
-        var pointH: Int = 0
-        var pointV: Int = 0
-        
-        btnCancel = findViewById(R.id.btnCancel)
-        btnAccept = findViewById(R.id.btnAccept)
 
         btnCancel.setOnClickListener {
             View.OnClickListener {
@@ -101,13 +97,17 @@ class soccer_scoreboard : AppCompatActivity() {
             }
         }
 
-        /**
-         * Este botón de aquí abajo es el de aceptar, cuando vos los presiones vas a triggerear los siguiente:
-         * Primero va a construir un objeto POJO de tipo MatchResponse que yo construí, ahí le das a la referencia del mismo
-         * Segundo, el correrá una función para revisar si hay internet
-         */
-
         btnAccept.setOnClickListener {
+            try {
+                pointH = tieScoreH.text.toString().toInt()
+                pointV = tieScoreV.text.toString().toInt()
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(
+                    applicationContext,
+                    "Por favor escribir un número valido, sin caracteres especiales",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
             val bodyResponse: MatchRequest = MatchRequest(
                 "2021-11-08T05:38:09.305Z",
                 pointH,
@@ -118,87 +118,40 @@ class soccer_scoreboard : AppCompatActivity() {
             )
             if(isNetworkConnected())
                 sendScore(bodyResponse)
-            else
-                println("No internet")
+            else {
+                Toast.makeText(
+                    applicationContext,
+                    "No hay conexión de internet en este momento, favor revisar su conexión o intente más tarde",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
-
-    /**
-     * Esta función de aquí será nuestra "corrutina" que es un concepto que kotlin agregó con el fin
-     * de aligerar el trabajo asíncrono. Entonces, lo que pase dentro del CoroutineScope será en un hilo
-     * asíncrono de la aplicación. Lo que pase dentro del runOnUiThread será en el hilo primario de la aplicación.
-     *
-     * Sigamos...
-     *
-     * Lo primero que vamos a pedirle es que me devuelva una entidad de tipo Retrofit (que es con la que estoy trabajando
-     * para las peticiones HTTP) usando el método de más abajo de getRetrofit().
-     *
-     * Segundo, una vez obtenido el objeto Retrofit, le digo con el método create() que construya una entidad de tipó
-     * APIServiceMatch (es una interface construida por mí persona, la podemos visualizar como una especie de
-     * controlador o handler de archivos) y llamamos al método sendDataMacht() que no es más que un método de la
-     * interface que se encarga de hacer peticiones de tipo, se le debe pasar 3 datos:
-     *
-     * El token de autorización
-     * El endpoint
-     * El POJO (que en este caso generamos más arriba)
-     *
-     * Por ultimo...
-     *
-     * Tendremos una variable que se llama result, su objetivo es almacenar el cuerpo o body() de nuestra
-     * petición y de ahí en más es lo que queda en nuestro hilo primario.
-     */
 
     private fun sendScore(bodyResponse: MatchRequest) {
 
         var gson = Gson()
         var jsonString = gson.toJson(bodyResponse)
         println(jsonString)
-            apiClient.getApiService().postMatch(token = "Bearer ${sessionManager.fetchAuthToken()}", bodyResponse)
+        apiClient.getApiService()
+            .postMatch(token = "Bearer ${sessionManager.fetchAuthToken()}", bodyResponse)
             .enqueue(object : Callback<MatchResponce> {
                 override fun onFailure(call: Call<MatchResponce>, t: Throwable) {
-
-                    //TODO: ESTA VARA FUNCIONA LA VARA ES QUE PORQUE LOS POJOS DE TOURNAMENT Y FIELD NO TIENEN LOS CAMPOS NESESARIOS CORREJIT PARA EVITAR EL ERROR
                     println(call)
                     println(t)
                     println("Error")
                 }
 
-                override fun onResponse(call: Call<MatchResponce>, response: Response<MatchResponce>) {
-                    // Handle function to display posts
-                    println(call)
-                    println(response)
+                override fun onResponse(
+                    call: Call<MatchResponce>,
+                    response: Response<MatchResponce>
+                ) {
                     runOnUiThread {
-                        println("All good")
-                        tvCounterH.setText("0")
-                        tvCounterH.setText("0")
+                        tieScoreH.refreshDrawableState()
+                        tieScoreV.refreshDrawableState()
                     }
                 }
             })
-
-
-        /*
-        CoroutineScope(Dispatchers.IO).launch {
-            val call = getRetrofit()
-                .create(APIServiceMatch::class.java)
-                .sendDataMacht(
-                    token,
-                    "matches",
-                    bodyResponse
-                )
-
-            val result = call.body()
-
-            println(call)
-
-            runOnUiThread {
-                if (call.isSuccessful) {
-
-                } else {
-                    showError()
-                }
-            }
-
-        } */
     }
 
     /**
@@ -210,7 +163,6 @@ class soccer_scoreboard : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
-
 
     /**
      * Este método checkea sí hay red
