9e69063f48dae74837ce6b1d283045e5024e3c86
theonlyarchitect <cmoralesr@ucenfotec.ac.cr>
2021-11-09 23:17:47 -0600

    Removed unnesesary files

--- app/src/main/java/com/mobile/heroes/mytournament/FeedResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.mobile.heroes.mytournament
-
-import com.google.gson.annotations.SerializedName
-
-data class FeedResponse(
-    @SerializedName("status") var status: String,
-    @SerializedName("message") var images: List<String>
-)
