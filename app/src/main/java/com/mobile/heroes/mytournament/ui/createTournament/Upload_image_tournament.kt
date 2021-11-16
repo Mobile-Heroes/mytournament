d27d688f2c07e3893cf624a0c5796a96cbb5ae91
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2021-11-15 20:51:21 -0600

    Finalización visual
    Se finalizó con la parte visual de la fase de subir la foto del perfil del torneo.

--- app/src/main/java/com/mobile/heroes/mytournament/ui/createTournament/Upload_image_tournament.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/ui/createTournament/Upload_image_tournament.kt
@@ -1,10 +1,33 @@
 package com.mobile.heroes.mytournament.ui.createTournament
 
+import SessionManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.gson.Gson
 import com.mobile.heroes.mytournament.R
+import com.mobile.heroes.mytournament.networking.ApiClient
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class upload_image_tournament : AppCompatActivity() {
+
+    private lateinit var sessionManager: SessionManager
+    private lateinit var apiClient: ApiClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload_image_tournament)
