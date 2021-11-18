8caae7c3d0a60e27fa19a8e4c99dc3c88576a826
rsanchezc <rsanchezc@ucenfotec.ac.cr>
2021-11-18 13:29:53 -0600

    added tournament team profile changes info

--- app/src/main/java/com/mobile/heroes/mytournament/ProfileTournamentTeam.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/ProfileTournamentTeam.kt
@@ -18,9 +18,24 @@ class ProfileTournamentTeam : AppCompatActivity() {
     fun changeTeamProfileInfo(){
         val bundle = intent.extras
         val profileName = bundle?.get("INTENT_NAME")
+        val profilePoints = bundle?.get("INTENT_POINTS")
+        val profileGoalsDone = bundle?.get("INTENT_GOALS_DONE")
+        val profileGoalsReceived = bundle?.get("INTENT_GOALS_RECEIVED")
+        val profilePicture = bundle?.get("INTENT_TEAM_PICTURE")
 
         var profileNameTextView : TextView = findViewById(R.id.tv_tournament_team_profile_name)
         profileNameTextView.setText("$profileName")
+
+        var profilePointsTextView : TextView = findViewById(R.id.tv_tournament_team_profile_points_value)
+        profilePointsTextView.setText("$profilePoints")
+
+        var profileGoalsDoneTextView : TextView = findViewById(R.id.tv_tournament_team_profile_goals_value)
+        profileGoalsDoneTextView.setText("$profileGoalsDone")
+
+/*        val imageBytes = Base64.decode("$profileIcon",0)
+        val image = BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.size)*/
+        var profilePictureImageView : ImageView = findViewById(R.id.iv_tournament_team_profile_head_image)
+        profilePictureImageView.setImageResource(R.drawable.ic_form_tournament_name)
 
     }
 
