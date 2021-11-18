08ef5ccdbda014ab078d56a81c48007ababa53a4
rsanchezc <rsanchezc@ucenfotec.ac.cr>
2021-11-18 15:39:59 -0600

    layout general table row item

---
+++ app/src/main/java/com/mobile/heroes/mytournament/ProfileTournamentTable.kt
@@ -0,0 +1,11 @@
+package com.mobile.heroes.mytournament
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileTournamentTable : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_tournament_table)
+    }
+}
