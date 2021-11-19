e67d315adb400eb32986d66c77d29a227fa29cab
rsanchezc <rsanchezc@ucenfotec.ac.cr>
2021-11-18 18:39:56 -0600

    added recicler view tournament table

--- app/src/main/java/com/mobile/heroes/mytournament/ProfileTournamentTable.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/ProfileTournamentTable.kt
@@ -3,23 +3,24 @@ package com.mobile.heroes.mytournament
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mobile.heroes.mytournament.tournamentprofile.TournamentTableAdapter
+import kotlinx.android.synthetic.main.tournament_table_body_center.*
 
 class ProfileTournamentTable : AppCompatActivity() {
 
     private var teamNameList = mutableListOf<String>()
-    private var teamImageList = mutableListOf<Int>()
+    private var teamPositionList = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_tournament_table)
 
         changeInfo()
-
-
-
-
-
-
+        postToTanbleList()
+        
+        rv_tournament_table.layoutManager = LinearLayoutManager(this)
+        rv_tournament_table.adapter = TournamentTableAdapter(teamPositionList,teamNameList )
     }
 
     fun changeInfo(){
@@ -32,14 +33,14 @@ class ProfileTournamentTable : AppCompatActivity() {
 
     }
 
-    private fun addToList(name:String, image:Int){
-        teamImageList.add(image)
+    private fun addToList(position:String, name:String){
         teamNameList.add(name)
+        teamPositionList.add(position)
     }
 
-    private fun postToTeamList(){
+    private fun postToTanbleList(){
         for(i:Int in 1..8){
-            addToList("Equipo $i",  R.drawable.ic_form_tournament_name)
+            addToList(i.toString(), "Equipo $i")
         }
     }
 
--- app/src/main/java/com/mobile/heroes/mytournament/tournamentprofile/TournamentTableAdapter.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/tournamentprofile/TournamentTableAdapter.kt
@@ -1,4 +1,40 @@
 package com.mobile.heroes.mytournament.tournamentprofile
 
-class TournamentTableAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mobile.heroes.mytournament.R
+import kotlin.random.Random
+
+class TournamentTableAdapter(private var posicion: List<String>, private var names: List<String>) :
+    RecyclerView.Adapter<TournamentTableViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TournamentTableViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return TournamentTableViewHolder(layoutInflater.inflate(R.layout.item_table_row, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return names.size
+    }
+
+    override fun onBindViewHolder(holder: TournamentTableViewHolder, position: Int) {
+
+        val goalsDoneRandomValue = Random.nextInt(0,15)
+        val goalsRecievedRandomValue = Random.nextInt(0,15)
+        val goalsDif = goalsDoneRandomValue - goalsRecievedRandomValue
+        val pointsRandomValue = Random.nextInt(0,30)
+
+        //holder.itemPicture.setImageResource(images[position])
+        holder.itemPosition.text = posicion[position]
+        holder.itemPicture.setImageResource(R.drawable.ic_form_tournament_name)
+        holder.itemName.text = names[position]
+        holder.itemGoalsDone.text = goalsDoneRandomValue.toString()
+        holder.itemGoalsReceived.text = goalsRecievedRandomValue.toString()
+        holder.itemGoalsDif.text = goalsDif.toString()
+        holder.itemPoints.text =  pointsRandomValue.toString()
+
+    }
+
 }
