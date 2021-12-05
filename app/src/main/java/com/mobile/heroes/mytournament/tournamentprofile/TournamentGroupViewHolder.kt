d376fc82e95434f5db27d53fff11629c589a9043
rsanchezc <rsanchezc@ucenfotec.ac.cr>
2021-12-04 22:12:50 -0600

    added tournament group view holder

---
+++ app/src/main/java/com/mobile/heroes/mytournament/tournamentprofile/TournamentGroupViewHolder.kt
@@ -0,0 +1,48 @@
+package com.mobile.heroes.mytournament.tournamentprofile
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mobile.heroes.mytournament.R
+
+class TournamentGroupViewHolder (view: View): RecyclerView.ViewHolder(view) {
+
+    val itemGroupName: TextView = itemView.findViewById(R.id.tv_tournament_group_card_name_title)
+
+    val itemPositionRow1: TextView = itemView.findViewById(R.id.tv_tournament_table_card_position_row_1)
+    val itemPictureRow1: ImageView = itemView.findViewById(R.id.iv_tournament_table_card_image_row_1)
+    val itemNameRow1: TextView = itemView.findViewById(R.id.tv_tournament_table_card_name_row_1)
+    val itemMatchesRow1: TextView = itemView.findViewById(R.id.tv_tournament_table_card_matches_row_1)
+    val itemGoalsDoneRow1: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_done_row_1)
+    val itemGoalsReceivedRow1: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_received_row_1)
+    val itemGoalsDifRow1: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_dif_row_1)
+    val itemPointsRow1: TextView = itemView.findViewById(R.id.tv_tournament_table_card_points_row_1)
+
+    val itemPositionRow2: TextView = itemView.findViewById(R.id.tv_tournament_table_card_position_row_2)
+    val itemPictureRow2: ImageView = itemView.findViewById(R.id.iv_tournament_table_card_image_row_2)
+    val itemNameRow2: TextView = itemView.findViewById(R.id.tv_tournament_table_card_name_row_2)
+    val itemMatchesRow2: TextView = itemView.findViewById(R.id.tv_tournament_table_card_matches_row_2)
+    val itemGoalsDoneRow2: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_done_row_2)
+    val itemGoalsReceivedRow2: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_received_row_2)
+    val itemGoalsDifRow2: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_dif_row_2)
+    val itemPointsRow2: TextView = itemView.findViewById(R.id.tv_tournament_table_card_points_row_2)
+
+    val itemPositionRow3: TextView = itemView.findViewById(R.id.tv_tournament_table_card_position_row_3)
+    val itemPictureRow3: ImageView = itemView.findViewById(R.id.iv_tournament_table_card_image_row_3)
+    val itemNameRow3: TextView = itemView.findViewById(R.id.tv_tournament_table_card_name_row_3)
+    val itemMatchesRow3: TextView = itemView.findViewById(R.id.tv_tournament_table_card_matches_row_3)
+    val itemGoalsDoneRow3: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_done_row_3)
+    val itemGoalsReceivedRow3: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_received_row_3)
+    val itemGoalsDifRow3: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_dif_row_3)
+    val itemPointsRow3: TextView = itemView.findViewById(R.id.tv_tournament_table_card_points_row_3)
+
+    val itemPositionRow4: TextView = itemView.findViewById(R.id.tv_tournament_table_card_position_row_4)
+    val itemPictureRow4: ImageView = itemView.findViewById(R.id.iv_tournament_table_card_image_row_4)
+    val itemNameRow4: TextView = itemView.findViewById(R.id.tv_tournament_table_card_name_row_4)
+    val itemMatchesRow4: TextView = itemView.findViewById(R.id.tv_tournament_table_card_matches_row_4)
+    val itemGoalsDoneRow4: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_done_row_4)
+    val itemGoalsReceivedRow4: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_received_row_4)
+    val itemGoalsDifRow4: TextView = itemView.findViewById(R.id.tv_tournament_table_card_goals_dif_row_4)
+    val itemPointsRow4: TextView = itemView.findViewById(R.id.tv_tournament_table_card_points_row_4)
+}
