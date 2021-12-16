b8a18708fc8fa93ce399edb77a1f9fc666230518
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2021-12-16 00:59:17 -0600

    Cambio de ultimo momento

--- app/src/main/java/com/mobile/heroes/mytournament/tournamentprofile/TournamentProfileTeamViewHolder.kt
+++ app/src/main/java/com/mobile/heroes/mytournament/tournamentprofile/TournamentProfileTeamViewHolder.kt
@@ -9,7 +9,7 @@ import com.mobile.heroes.mytournament.R
 
 class TournamentProfileTeamViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
-    val itemPicture: ImageView = itemView.findViewById(R.id.iv_soccer_field_card_image)
+    val itemPicture: ImageView = itemView.findViewById(R.id.iv_tournament_team_card_image)
     val itemName: TextView = itemView.findViewById(R.id.tv_tournament_team_card_name)
     val itemTitles: TextView = itemView.findViewById(R.id.tv_tournament_team_card_titles_value)
     val itemDelete: ImageButton = itemView.findViewById(R.id.bt_delete_team_tournament)
